1ca55fbaf1c2c0020eccff063042e38f78373513
reata <reddevil.hjw@gmail.com>
2021-10-03 14:34:45 +0800

    feat: new gist on python conditional expression

--- gatsby-config.js
+++ gatsby-config.js
@@ -284,6 +284,10 @@ module.exports = {
     ],
     gists: [
       {
+        title: "Python的条件表达式等价于if else代码块吗？",
+        link: "https://gist.github.com/reata/39684356cbcbdc14d94b9fc9a1db8d8c"
+      },
+      {
         title: "在Python中查询MySQL是CPU密集型还是IO密集型任务，能用多线程来加速吗？",
         link: "https://gist.github.com/reata/7894016938bc4110bb5516cdd87fe1a0"
       }
